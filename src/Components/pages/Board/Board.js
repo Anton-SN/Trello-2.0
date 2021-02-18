151ef36d7da7c05f0598572bf6a51bfe8243bef7
Anton Sivagin <ansn.onward@gmail.com>
2021-02-18 19:16:08 +0300

    feat: add drag and drop

--- src/Components/pages/Board/Board.js
+++ src/Components/pages/Board/Board.js
@@ -3,32 +3,61 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Column from './Components/Column/Column';
 import Note from './Components/Note/Note';
 import styles from './Board.module.css';
 
-const Board = ({ id: boardId, columns }) => (
+const Board = ({ boardId, columns }) => (
   <Paper elevation={3} className={styles.container}>
     <Typography component="h3" variant="h3" className={styles.title}>
       Board
     </Typography>
     <div className={styles.columnContainer} key={boardId}>
-      {columns.map(({ id: columnId, name, notes }) => (
-        <Column key={columnId} title={name}>
-          {notes.map(({ id: noteId, title }) => (
-            <Note title={title} key={noteId} />
-          ))}
-        </Column>
+      {columns.map(({ columnId, name, notes }) => (
+        <DragDropContext>
+          <Droppable droppableId={`column-${columnId}`}>
+            {(provided) => (
+              <div
+                key={columnId}
+                className={`column-${columnId}`}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <Column title={name}>
+                  {notes.map(({ noteId, title }, index) => (
+                    <Draggable
+                      key={`${columnId}-${noteId}`}
+                      draggableId={`${columnId}-${noteId}`}
+                      index={index}
+                    >
+                      {/* eslint-disable-next-line no-shadow */}
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <Note title={title} />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </Column>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       ))}
     </div>
   </Paper>
 );
 
 Board.propTypes = {
-  id: PropTypes.number.isRequired,
+  boardId: PropTypes.number.isRequired,
   columns: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      columnId: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       notes: PropTypes.arrayOf(PropTypes.shape({})),
     }),
@@ -40,7 +69,7 @@ Board.defaultProps = {
 };
 
 const mapState = (state) => ({
-  id: state.board.id || 0,
+  boardId: state.board.boardId || 0,
   columns: state.board.columns || [],
 });
 
